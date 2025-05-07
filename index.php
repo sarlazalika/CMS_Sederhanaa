f1402f2aa2d2240a978463e651f103989e4ec270
sarlazalika <sarlazalika@gmail.com>
2025-05-07 20:36:50 +0700

    Membuat File Index.php

--- index.php
+++ index.php
@@ -86,4 +86,4 @@ if (!isset($_SESSION['login'])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 </body>
-</html>
+</html> 
